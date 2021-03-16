ecdb9880a3c0ae64788b3a4b95651b5fbd328cce
Marina Santana <marinasantana7894@gmail.com>
2021-03-15 22:18:51 -0300

    feat: add favico and title

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ function Home({pokemon}) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet"/>      
+        <title>Pokemon Galerry</title>
       </Head>
       <body>
         <div className="flex flex-col">
